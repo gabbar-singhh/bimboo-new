67fe5545a5a20a73cd142b590f42228b79149e2f
gabbar-singhh <himanshup1308@gmail.com>
2025-04-18 01:24:48 +0530

    nav hero brands -working

---
+++ Components/BrandsWorked/BrandsWorked.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styles from "./BrandsWorked.module.css";
+
+const BrandsWorked = () => {
+  return (
+    <div className={styles.brandsMain}>
+      <h2>
+        Worked with brands across various projects, collabs & volunteer
+        initiatives
+      </h2>
+
+      <div className={styles.logos}>
+        <div className={styles.logosSlide}>
+          <img src={`/brands/1.svg`} alt={"brands logo"} height={"64px"} />{" "}
+          <img src={`/brands/2.svg`} alt={"brands logo"} height={"68px"} />{" "}
+          <img src={`/brands/3.svg`} alt={"brands logo"} height={"64px"} />{" "}
+          <img src={`/brands/4.svg`} alt={"brands logo"} height={"68px"} />{" "}
+          <img
+            src={`/brands/5.svg`}
+            alt={"brands logo"}
+            height={"72px"}
+            width={"auto"}
+          />{" "}
+          <img src={`/brands/6.svg`} alt={"brands logo"} height={"84px"} />
+          <img src={`/brands/1.svg`} alt={"brands logo"} height={"64px"} />{" "}
+          <img src={`/brands/2.svg`} alt={"brands logo"} height={"68px"} />{" "}
+          <img src={`/brands/3.svg`} alt={"brands logo"} height={"64px"} />{" "}
+          <img src={`/brands/4.svg`} alt={"brands logo"} height={"68px"} />{" "}
+          <img
+            src={`/brands/5.svg`}
+            alt={"brands logo"}
+            height={"72px"}
+            width={"auto"}
+          />{" "}
+          <img src={`/brands/6.svg`} alt={"brands logo"} height={"84px"} />
+          <img src={`/brands/1.svg`} alt={"brands logo"} height={"64px"} />{" "}
+          <img src={`/brands/2.svg`} alt={"brands logo"} height={"68px"} />{" "}
+          <img src={`/brands/3.svg`} alt={"brands logo"} height={"64px"} />{" "}
+          <img src={`/brands/4.svg`} alt={"brands logo"} height={"68px"} />{" "}
+          <img
+            src={`/brands/5.svg`}
+            alt={"brands logo"}
+            height={"72px"}
+            width={"auto"}
+          />{" "}
+          <img src={`/brands/6.svg`} alt={"brands logo"} height={"84px"} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BrandsWorked;
---
+++ Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./HeroSection.module.css";
+
+const HeroSection = () => {
+  return (
+    <section className={styles.heroMain}>
+      <div class={styles.blurredOverlay}></div>
+      <nav className={styles.navMain}>
+        <div className={styles.logoImg}>
+          <img src="/logo-type.svg" alt="logo" height={"28px"} width={"auto"} />
+        </div>
+        <ul>
+          <li>Services</li>
+          <li>Process</li>
+          <li>Case Study</li>
+          <li>Testimonials</li>
+        </ul>
+      </nav>
+
+      <div className={styles.heroContainer}>
+        <h1>
+          We build high converting website for <br /> Product & Software
+          companies
+        </h1>
+        <p>
+          We build websites for Product & Software companies which predictably
+          bring them more leads and build trust in their market segment
+        </p>
+        <div className={styles.ctaButton}>
+          Book a Free Discovery Call{" "}
+          <img
+            src="/icons/arrow-right.svg"
+            alt="arrow right"
+            height={"26px"}
+            width={"auto"}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
