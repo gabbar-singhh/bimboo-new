4da1833b487e3e6ede754a474c1d7142580474da
gabbar-singhh <himanshup1308@gmail.com>
2025-04-22 11:11:48 +0530

    brands horizontall scroll -done

--- Components/BrandsWorked/BrandsWorked.jsx
+++ Components/BrandsWorked/BrandsWorked.jsx
@@ -1,48 +1,28 @@
 import React from "react";
 import styles from "./BrandsWorked.module.css";
 
+const logos = [
+  "/brands/1.svg",
+  "/brands/2.svg",
+  "/brands/3.svg",
+  "/brands/4.svg",
+  "/brands/5.svg",
+  "/brands/6.svg",
+];
+
 const BrandsWorked = () => {
   return (
     <div className={styles.brandsMain}>
       <h2>
         Worked with brands across various projects, collabs & other initiatives
       </h2>
-
-      <div className={styles.logos}>
-        <div className={styles.logosSlide}>
-          <img src={`/brands/1.svg`} alt={"brands logo"} height={"64px"} />{" "}
-          <img src={`/brands/2.svg`} alt={"brands logo"} height={"68px"} />{" "}
-          <img src={`/brands/3.svg`} alt={"brands logo"} height={"64px"} />{" "}
-          <img src={`/brands/4.svg`} alt={"brands logo"} height={"68px"} />{" "}
-          <img
-            src={`/brands/5.svg`}
-            alt={"brands logo"}
-            height={"72px"}
-            width={"auto"}
-          />{" "}
-          <img src={`/brands/6.svg`} alt={"brands logo"} height={"84px"} />
-          <img src={`/brands/1.svg`} alt={"brands logo"} height={"64px"} />{" "}
-          <img src={`/brands/2.svg`} alt={"brands logo"} height={"68px"} />{" "}
-          <img src={`/brands/3.svg`} alt={"brands logo"} height={"64px"} />{" "}
-          <img src={`/brands/4.svg`} alt={"brands logo"} height={"68px"} />{" "}
-          <img
-            src={`/brands/5.svg`}
-            alt={"brands logo"}
-            height={"72px"}
-            width={"auto"}
-          />{" "}
-          <img src={`/brands/6.svg`} alt={"brands logo"} height={"84px"} />
-          <img src={`/brands/1.svg`} alt={"brands logo"} height={"64px"} />{" "}
-          <img src={`/brands/2.svg`} alt={"brands logo"} height={"68px"} />{" "}
-          <img src={`/brands/3.svg`} alt={"brands logo"} height={"64px"} />{" "}
-          <img src={`/brands/4.svg`} alt={"brands logo"} height={"68px"} />{" "}
-          <img
-            src={`/brands/5.svg`}
-            alt={"brands logo"}
-            height={"72px"}
-            width={"auto"}
-          />{" "}
-          <img src={`/brands/6.svg`} alt={"brands logo"} height={"84px"} />
+      <div className={styles.sliderWrapper}>
+        <div className={styles.sliderTrack}>
+          {[...logos, ...logos].map((logo, idx) => (
+            <div className={styles.logoItem} key={idx}>
+              <img src={logo} alt={`logo-${idx}`} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
